ce72a390f1599e7624b37387f3eca15d2a352acd
Shoval T <shoval.tirman@gmail.com>
2011-11-16 13:52:40 +0200

    completed pre-lab3

---
+++ lab3/bcmp/bcmp.c
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#define BUFFER_SIZE 1
+
+int main(int argc, char *argv[]) {
+  FILE* f1;
+  FILE* f2;
+  unsigned char buf1[BUFFER_SIZE];
+  unsigned char buf2[BUFFER_SIZE];
+
+  if (argc != 3) {
+    printf("Insufficient arguments\n");
+    return 0;
+  }
+
+  f1 = fopen(argv[1], "rb");
+  f2 = fopen(argv[2], "rb");
+
+  if (f1 == NULL || f2 == NULL) {
+    printf("Unable to read files\n");
+    return 0;
+  }
+
+  int count = 0;
+  int found = 0;
+
+  while(fread(buf1, BUFFER_SIZE, 1, f1)) {
+    if (fread(buf2, BUFFER_SIZE, 1, f2)) {
+      if (buf1[0] != buf2[0]) {
+        found = 1;
+        printf("byte %i -%i +%i\n", count, buf1[0], buf2[0]);
+        break;
+      }
+    } else {
+      // file 2 is shorter than file 1
+      found = 1;
+      printf("Reached end of file %s\n", argv[2]);
+      break;
+    }
+    ++count;
+  }
+
+  if (!found) {
+    if (fread(buf2, BUFFER_SIZE, 1, f2)) {
+      // file 1 is shorter than file 2
+      found = 1;
+      printf("Reached end of file %s\n", argv[1]);
+    } else {
+      // no differences found
+      printf("Files are identical\n");
+    }
+  }
+
+  fclose(f1);
+  fclose(f2);
+  return 0;
+}

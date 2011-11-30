9a903c8ec8560baf9de77281a51da9712e513777
Shoval T <shoval.tirman@gmail.com>
2011-11-30 16:49:00 +0200

    lab 5

--- lab5/ahalan/ahalan.asm
+++ lab5/ahalan/ahalan.asm
@@ -1,18 +1,28 @@
 section	.text
 	global ahalan
+	global write
+	extern myputs
 
 ahalan:					;void ahalan()
-	push ebp
-	mov ebp, esp
-	mov edx,len
-	mov	ecx,msg	;message to write
-	mov	ebx,1	;file descriptor (stdout)
-	mov	eax,4	;system call number (sys_write)
+	push	msg
+	call	myputs
+	;ret
+	mov	eax,1	;system call number (sys_exit)
 	int	0x80	;call kernel
-	pop ebp
-	ret
+
+write:
+	push	ebp
+	mov 	ebp,esp
+	mov 	edx,[ebp+16]
+	mov 	ecx,[ebp+12]	;message to write
+	mov 	ebx,[ebp+8]	;file descriptor (stdout)
+	mov 	eax,4	;system call number (sys_write)
+	int 	0x80	;call kernel
+	pop 	ebp
+	;ret
+	mov	eax,1	;system call number (sys_exit)
+	int	0x80	;call kernel
 
 section	.data
 
-msg	db	'ahalan!',0xa,0	;our dear string
-len equ $ - msg
+msg	db	'ahalan!',0xa	;our dear string
--- lab5/ahalan/main.c
+++ lab5/ahalan/main.c
@@ -1,6 +1,17 @@
 void ahalan();
+void write();
 
 int main(int arg, char **argv) {
 	ahalan();
 	return 0;
+}
+
+int myputs(char *s) {
+  write(1, s, length(s));
+}
+
+int length(char *s) {
+  char *t = s;
+  while(*t) ++t;
+  return t-s;
 }

64c582bbf6ff3286652af147507b59c67635aaf9
Shoval T <shoval.tirman@gmail.com>
2012-01-18 13:27:53 +0200

    finished printing section headers

--- lab9/elfheader/elfheader.c
+++ lab9/elfheader/elfheader.c
@@ -2,21 +2,36 @@
 	#include <string.h>
 	#include <stdio.h>
 	#include <elf.h>
- 
+	#include <unistd.h>
+
 	int main(int argc, char **argv) {
-		int read;
-		
-		if (argc < 2) { 
-			printf("Insufficient arguments (expected: 1; found: %i)\n", argc);
+		int read, opt, showsect, showsymb;
+
+		showsect = 0;
+		showsymb = 0;
+		while ((opt = getopt(argc, argv, "sS")) != -1) {
+			switch (opt) {
+			  case 's':
+				showsymb = 1;
+				break;
+			  case 'S':
+				showsect = 1;
+				break;
+			}
+		}
+
+		if (argc-optind < 1) {
+			// files not specified
+			printf("Insufficient arguments\n");
 			return 1;
 		}
-		
+
 		FILE* f;
-		if (!(f = fopen(argv[1], "r"))) {
+		if (!(f = fopen(argv[optind], "r"))) {
 			printf("Unable to open file: %s\n", argv[1]);
 			return 1;
 		}
-		
+
 		// verify type is 32 bit
 		unsigned char	ident[EI_NIDENT];	/* Magic number and other info */
 		if (!fread(&ident, sizeof(ident), 1, f)) {
@@ -26,7 +41,7 @@
 			printf("Invalid class (Expecting only 32-bit objects)\n");
 			return 1;
 		}
-		
+
 		// read header
 		Elf32_Ehdr header;
 		rewind(f);
@@ -34,13 +49,13 @@
 			printf("Unable to read file: %s\n", argv[1]);
 			return 1;
 		}
-		
+
 		int i = 0;
 		char str_ident[48];
 		for (i=0; i<EI_NIDENT; ++i) {
 			sprintf(str_ident + (i*3), "%02x ", header.e_ident[i]);
 		}
-		
+
 		printf("ELF Header:\n");
 		printf("  Magic:   %s\n", str_ident);
 		printf("  Class:                             %i\n", header.e_ident[4]);
@@ -61,26 +76,32 @@
 		printf("  Size of section headers:           %i (bytes)\n", header.e_shentsize);
 		printf("  Number of section headers:         %i\n", header.e_shnum);
 		printf("  Section header string table index: %i\n", header.e_shstrndx);
-		
-		Elf32_Shdr shdr;
-		int nameoff;
-		int namesize;
-		fseek(f, header.e_shoff + (header.e_shentsize * header.e_shstrndx), SEEK_SET);
-		read = fread(&shdr, sizeof(shdr), 1, f);
-		nameoff = shdr.sh_offset;
-		namesize = shdr.sh_size;
-		
-		char* names = malloc(namesize);
-		fseek(f, nameoff, SEEK_SET);
-		read = fread(names, namesize, 1, f);
-		
-		printf("\n");
-		for (i=0; i<header.e_shnum; i++) {
-			fseek(f, header.e_shoff + (header.e_shentsize * i), SEEK_SET);
+
+		if (showsect) {
+			Elf32_Shdr shdr;
+			int nameoff;
+			int namesize;
+			fseek(f, header.e_shoff + (header.e_shentsize * header.e_shstrndx), SEEK_SET);
 			read = fread(&shdr, sizeof(shdr), 1, f);
-			printf("[%02i] name: %s; offset: %06x; size: %06x; type: %i;\n", i, names + shdr.sh_name, shdr.sh_offset, shdr.sh_size, shdr.sh_type);
+			nameoff = shdr.sh_offset;
+			namesize = shdr.sh_size;
+
+			char* names = malloc(namesize);
+			fseek(f, nameoff, SEEK_SET);
+			read = fread(names, namesize, 1, f);
+
+			printf("\nSection Headers:\n  #\tName                          \tOffset\tSize\tType\n");
+			for (i=0; i<header.e_shnum; i++) {
+				fseek(f, header.e_shoff + (header.e_shentsize * i), SEEK_SET);
+				read = fread(&shdr, sizeof(shdr), 1, f);
+				printf("  [%02i]\t%-30s\t%06x\t%06x\t%i\n", i, names + shdr.sh_name, shdr.sh_offset, shdr.sh_size, shdr.sh_type);
+			}
+
+			free(names);
 		}
-		
-		free(names);
-		return 0;                 
+
+		if (showsymb) {
+			printf("\nSymbol Table:\n  [Not Implemented]\n");
+		}
+		return 0;
  }
